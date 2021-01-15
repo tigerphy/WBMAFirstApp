5fc81c056142061f09124b289ac803561dce6c69
Chingman <chingman.lai@metropolia.fi>
2021-01-15 09:58:16 +0200

    ajax task A ready

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import List from './components/List';
 import GlobalStyles from './utils/GlobalStyles';
 
--- components/List.js
+++ components/List.js
@@ -1,41 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, TouchableOpacity, Image, FlatList } from "react-native";
-import ListItem from "./ListItem";
+import ListItem from './ListItem';
 
-const mediaArray = [
-  {
-    'key': '0',
-    'title': 'Title 1',
-    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/161',
-    },
-    'filename': 'http://placekitten.com/2048/1920',
-  },
-  {
-    'key': '1',
-    'title': 'Title 2',
-    'description': 'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/164',
-    },
-    'filename': 'http://placekitten.com/2041/1922',
-  },
-  {
-    'key': '2',
-    'title': 'Title 3',
-    'description': 'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    'thumbnails': {
-      w160: 'http://placekitten.com/160/167',
-    },
-    'filename': 'http://placekitten.com/2039/1920',
-  },
-];
+//fetch data with AJAX
+const url = 'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
 
 const List = () => {
+  //update the data to setMediaArray whenever the state is changed
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async () => {
+    try{
+    const response = await fetch(url);
+    //Store data in json
+    const json = await response.json();
+    console.log('response json data',json);
+    setMediaArray(json);
+  }catch (error){
+    console.error('loadMedia error', error);
+  }
+  };
+
+  //define when we want to react to this state)
+  useEffect(()=>{
+    loadMedia();
+  },[]);
+
+  
   return (
     <FlatList
       data={mediaArray}
+      keyExtractor={(item, index) => index.toString()}
       renderItem={({item}) => <ListItem singleMedia={item} />}
     />
   );
