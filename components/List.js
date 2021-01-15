200f5870dffbd0e40a3c1a6ece8347362e98de12
Chingman <chingman.lai@metropolia.fi>
2021-01-15 14:48:28 +0200

    update list.js

--- components/List.js
+++ components/List.js
@@ -1,42 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, Image, FlatList } from "react-native";
+import { FlatList } from "react-native";
 import ListItem from "./ListItem";
 
-//fetch data with AJAX
-const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
+const baseUrl ="http://media.mw.metropolia.fi/wbma"
 
 const List = () => {
-  //update the data to setMediaArray whenever the state is changed
   const [mediaArray, setMediaArray] = useState([]);
 
-  const loadMedia = async (limit = 5) => {
-    try {
-      const listResponse = await fetch(baseUrl + 'media?limit=' + limit);
-      //Store data in json
+  const loadMedia = async (limit =5) => {
+    try{
+      const listResponse = await fetch(baseUrl + "media?limit=" + limit);
       const listJson = await listResponse.json();
-      console.log('response json data', listJson);
-
-      //media is done after promise is finished
+      console.log("response json data", listJson);
       const media = await Promise.all(
-        listJson.map(async (item) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + item.file_id);
-          const fileJson = fileResponse.json();
-          //console.log("media file data", json);
-          return fileJson; //return file information
+        listJson.map(async (item)=> {
+          const fileResponse = await fetch(baseUrl + "media/" + item.file_id);
+          const fileJson=fileResponse.json();
+          return fileJson;
         })
-      ); //going through every item in an array
-      //setMediaArray(json);
-      console.log('media array data', media);
-
+      );
+      console.log("media array data", media);
+      
       setMediaArray(media);
-    } catch (error) {
-      console.error('loadMedia error', error);
+    }catch(error){
+      console.error("loadMedia error", error);
     }
   };
 
-  //define when we want to react to this state)
-  useEffect(() => {
-    loadMedia(5);
+  useEffect(()=>{
+    loadMedia(10);
   }, []);
 
   return (
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,14 +1,14 @@
 import React from "react";
-import { View, Text, TouchableOpacity, Image, StyleSheet, ImageBackground } from "react-native";
+import { View, Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 import PropTypes from "prop-types";
 
 const ListItem = (props) => {
   return (
     <TouchableOpacity style={styles.row}>
       <View style={styles.imagebox}>
-      <Image
-        style={styles.image}
-        source={{ uri: props.singleMedia.thumbnails.w160 }}
+        <Image
+          style={styles.image}
+          source={{ uri: props.singleMedia.thumbnails.w160 }}
         />
       </View>
       <View style={styles.textbox}>
@@ -24,31 +24,30 @@ ListItem.propTypes = {
 };
 
 const styles = StyleSheet.create({
-row:{
-flexDirection: 'row',
-padding: 15,
-marginBottom: 15,
-backgroundColor: '#eee',
-borderRadius: 16,
-},
-imagebox: {
-  flex:1,
-},
-image: {
-  flex: 1,
-  height: 150,
-  borderRadius: 6,
-  
-},
-textbox: {
-  flex: 1,
-  padding: 10,
-},
+  row: {
+    flexDirection: "row",
+    padding: 15,
+    marginBottom: 15,
+    backgroundColor: "#eee",
+    borderRadius: 16,
+  },
+  imagebox: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    height: 150,
+    borderRadius: 6,
+  },
+  textbox: {
+    flex: 1,
+    padding: 10,
+  },
   listTitle: {
     fontWeight: "bold",
     fontSize: 20,
     paddingBottom: 15,
-  }
+  },
 });
 
 export default ListItem;
