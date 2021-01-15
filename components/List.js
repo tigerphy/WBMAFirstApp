2d35b4eaccfd70fdf4ea1fd416360f1b8510a685
Chingman <chingman.lai@metropolia.fi>
2021-01-15 13:38:15 +0200

    AJAX + hooks

--- components/List.js
+++ components/List.js
@@ -1,43 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, TouchableOpacity, Image, FlatList } from "react-native";
-import ListItem from "./ListItem";
+import React from 'react';
+import {FlatList } from 'react-native';
+import {useLoadMedia} from '../hooks/ApiHooks';
+import ListItem from './ListItem';
 
-//fetch data with AJAX
-const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
 
 const List = () => {
-  //update the data to setMediaArray whenever the state is changed
-  const [mediaArray, setMediaArray] = useState([]);
-
-  const loadMedia = async (limit = 5) => {
-    try {
-      const listResponse = await fetch(baseUrl + 'media?limit=' + limit);
-      //Store data in json
-      const listJson = await listResponse.json();
-      console.log('response json data', listJson);
-
-      //media is done after promise is finished
-      const media = await Promise.all(
-        listJson.map(async (item) => {
-          const fileResponse = await fetch(baseUrl + 'media/' + item.file_id);
-          const fileJson = fileResponse.json();
-          //console.log("media file data", json);
-          return fileJson; //return file information
-        })
-      ); //going through every item in an array
-      //setMediaArray(json);
-      console.log('media array data', media);
-
-      setMediaArray(media);
-    } catch (error) {
-      console.error('loadMedia error', error);
-    }
-  };
-
-  //define when we want to react to this state)
-  useEffect(() => {
-    loadMedia(5);
-  }, []);
+  const [mediaArray] = useLoadMedia();
 
   return (
     <FlatList
---
+++ hooks/ApiHooks.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+//fetch data with AJAX
+const baseUrl = 'http://media.mw.metropolia.fi/wbma/';
+
+const useLoadMedia = () => {
+  //update the data to setMediaArray whenever the state is changed
+  const [mediaArray, setMediaArray] = useState([]);
+
+  const loadMedia = async (limit = 5) => {
+    try {
+      const listResponse = await fetch(baseUrl + 'media?limit=' + limit);
+      //Store data in json
+      const listJson = await listResponse.json();
+      console.log('response json data', listJson);
+
+      //media is done after promise is finished
+      const media = await Promise.all(
+        listJson.map(async (item) => {
+          const fileResponse = await fetch(baseUrl + 'media/' + item.file_id);
+          const fileJson = fileResponse.json();
+          //console.log("media file data", json);
+          return fileJson; //return file information
+        })
+      ); //going through every item in an array
+      //setMediaArray(json);
+      console.log('media array data', media);
+
+      setMediaArray(media);
+    } catch (error) {
+      console.error('loadMedia error', error);
+    }
+  };
+
+  useEffect(()=>{
+    loadMedia(10);
+  },[]);
+
+  return mediaArray;
+};
+
+export {useLoadMedia};
