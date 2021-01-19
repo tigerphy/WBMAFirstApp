be81ebcee4796b6df12c0e78f7191d95eecc5821
Chingman <chingman.lai@metropolia.fi>
2021-01-19 16:30:39 +0200

    layout

--- App.js
+++ App.js
@@ -1,26 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React from 'react';
-import { FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { header, StatusBar, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, footerArea, ImageBackground} from 'react-native';
 import List from './components/List';
 import GlobalStyles from './utils/GlobalStyles';
 
 
-const App = () => {
+const Flexboxdemo = () => {
   return (
-    <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <List />
-      <StatusBar style="auto"/>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor='orange' barStyle='light-content'/>
+
+      <View style={styles.header}>
+      <ImageBackground
+      source={require('./assets/original.jpg')}
+      style={styles.bgImage}
+      imageStyle={{borderRadius: 70}}
+      >
+        </ImageBackground>
+        <Text style={styles.hello}>Hello Stranger, hope you are ok</Text>
+        </View>
+
+      <View style={styles.infoArea}/>
+
+      <View style={styles.areaA}>
+        <Text>Hello World</Text> 
+      </View>
+
+      <View style={styles.areaB}>
+        <Text>AreaB</Text> 
+      </View>
+
+      <View style={styles.footerArea}/>
     </SafeAreaView>
   );
  };
 
+ export default Flexboxdemo;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    backgroundColor: 'yellow',
+    height: '100%',
+    paddingTop: 0,
   },
-});
+  header: {
+    height: 270,
+    backgroundColor: 'white',
+  },
+  bgImage:{
+    width: '100%',
+    height: 270,
+  },
+  hello: {
 
-export default App;
+  },
+  infoArea: {
+    flex: 6,
+    flexDirection: 'row',
+    backgroundColor: 'skyblue',
+  },
+  areaA:{
+    flex: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'pink',
+  },
+  footerArea: {
+    flex: 1,
+    backgroundColor: 'steelblue',
+  }
+
+});
